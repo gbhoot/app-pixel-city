858c91b15648672f03286b68fe9200b7dd29dd68
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-07-12 17:41:17 -0700

    added location services and current location region

--- pixel-city/pixel-city/Controller/MapVC.swift
+++ pixel-city/pixel-city/Controller/MapVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
@@ -17,10 +18,19 @@ class MapVC: UIViewController {
     @IBOutlet weak var headerLbl: UIView!
     @IBOutlet weak var currentLocationBtn: UIButton!
     
+    // Managers
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Delegats
         mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,12 +39,33 @@ class MapVC: UIViewController {
     }
 
     @IBAction func currentLocationBtnPressed(_ sender: Any) {
-        
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
 }
 
 // Does the same thing as inheriting the MKMapViewDelegate at the top in Class instance
 extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices()  {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
 }
 
---
+++ pixel-city/pixel-city/Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  pixel-city
+//
+//  Created by Gurpreet Bhoot on 7/12/18.
+//  Copyright © 2018 Gurpal Bhoot. All rights reserved.
+//
+
+import Foundation
+
